79b45161020ac394ce22b4fb9a711210e24bc5ba
Serhii Skorinov <sergii.skorinov@gmail.com>
2025-05-16 14:23:13 +0300

    feat: add function CreateAndPrintArray for task 1

--- Program.cs
+++ Program.cs
@@ -1,9 +1,24 @@
+using System;
+
 namespace C_Sharp_Homework
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            CreateAndPrintArray();
+        }
+        
+        // Task 1
+        static void CreateAndPrintArray()
+        {
+            int[] numbers = new int[50];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = (i + 1) * 2;
+                Console.WriteLine($"Element: {numbers[i]}");
+            }
         }
     }
 }
