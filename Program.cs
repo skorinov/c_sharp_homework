e4d1292b0a4ea169f66b2d69d3e73610def6a2b5
Serhii Skorinov <sergii.skorinov@gmail.com>
2025-05-16 14:26:01 +0300

    feat: add function CountEvenNumbersInArray for task 2

--- Program.cs
+++ Program.cs
@@ -1,9 +1,35 @@
+using System;
+using System.Collections.Generic;
+
 namespace C_Sharp_Homework
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            CountEvenNumbersInArray();
+        }
+        
+        // Task 2
+        static void CountEvenNumbersInArray()
+        {
+            Random random = new Random();
+            int[] numbers = new int[30];
+            List<int> eventList = new List<int>();
+            
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(0, 101);
+                if (numbers[i] % 2 == 0)
+                {
+                    eventList.Add(numbers[i]);
+                }
+            }
+
+            Console.WriteLine($"Array includes {eventList.Count} even numbers.");
+            Console.WriteLine(eventList.Count > 0
+                ? $"It's: {string.Join(", ", eventList)}"
+                : "There are no even numbers in the array.");
         }
     }
 }
