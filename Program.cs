e6b7a827a3737e931cb7fcd73f1fa002f739dbca
Serhii Skorinov <sergii.skorinov@gmail.com>
2025-05-16 14:31:07 +0300

    feat: add function PrintAlphabetAscii for task 4

--- Program.cs
+++ Program.cs
@@ -1,9 +1,29 @@
+using System;
+
 namespace C_Sharp_Homework
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            PrintAlphabetAscii();
+        }
+        
+        // Task 4
+        static void PrintAlphabetAscii()
+        {
+            int alphabetLength = 26; // a-z
+            char[] letters = new char[alphabetLength];
+
+            for (int i = 0; i < alphabetLength; i++)
+            {
+                letters[i] = (char)('a' + i);
+            }
+
+            foreach (char c in letters)
+            {
+                Console.WriteLine($"Character: {c}  |  ASCII code: {(int)c}");
+            }
         }
     }
 }
